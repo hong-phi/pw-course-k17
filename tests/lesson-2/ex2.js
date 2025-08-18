74f4e1b1d0cfa50d89b8889b67160427eeebfeac
PW-HongPhi <phitran1990@gmail.com>
2025-08-18 17:02:14 +0700

    lesson2/ex2.js

--- tests/lesson-2/ex2.js
+++ tests/lesson-2/ex2.js
@@ -1,3 +1,3 @@
 let myName = "Alex";
-myName = "Nagi";
+myName = "Nagi"; // chỗ sai là sử dụng biến const thì sẽ không thể gán lại giá trị cho biến này, sửa lại thành let để có thể gán giá trị lại cho biến
 console.log(myName);

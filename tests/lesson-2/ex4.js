e7ecb2a27d9360972f245752a48420587aa044bf
PW-HongPhi <phitran1990@gmail.com>
2025-08-18 16:56:21 +0700

    k17-lesson2-fix

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,5 +1,5 @@
-let hight = 170
-let eve = 70
+let hight = 170;
+let eve = hight %100;
 let weight_standar = (eve * 9) / 10;
 let weight = eve;
 let weight_min = (eve * 8) / 10;
--- tests/lesson-2/ex7.js
+++ tests/lesson-2/ex7.js
@@ -2,5 +2,4 @@ for (let i = 1000; i < 2001; i++) {
     if (i % 3 == 0) {
         console.log(i);
     }
-
 }
